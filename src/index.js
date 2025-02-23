b2a5a9b8dd3f9cabba970d7f7a27e533bbb80df6
Ana Cañete <ana.canete13@gmail.com>
2025-02-23 17:55:28 -0300

    Add margin to image

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
 
   let apiKey = "b9of4f791t3095ebe30890c6aaeec246";
-  let context =
-   "User instructions: You are an expert in romantic poems and you love writing short poems. Your mission is to generate a 4-line poem in basic HTML in <p> format and separate each line by adding this image /image/lavender.jpg in each separation in one <img> element except in the last space. Make sure to follow the user instructions. Write the poem directly, do not say that it is in html format and do not include titles. Sign the poem with 'Annx_C AI' inside an element<strong>at the end of the poem on a new line and NOT at the beginning." ;
+  let context = "User Instructions: You are an expert in romantic poems and you love writing short poems. Your mission is to generate a 4-line poem in basic HTML in <p> format and separate each line if or if with a break and adding this image /image/lavender.jpg at each separation in an <img> element except for the last space. Make sure to follow the user's instructions. Write the poem directly, do not say that it is in html format and do not include titles. Sign the poem with 'Annx_C AI' within an element<strong>at the end of the poem on a new line and NOT at the beginning." ;
   let prompt = `Generate a Spanish poem about ${instructionsInput.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
